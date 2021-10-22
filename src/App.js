31f791c5beda95129a009a899bdf754f1532d980
jinbest <jin.bestvictoria718@gmail.com>
2021-10-23 00:19:37 +0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import ReactFileReader from "react-file-reader";
-import { generateVariablesConditions } from "./utils/calculator";
+import {
+  generateVariablesConditions,
+  isEqualCondition,
+} from "./utils/calculator";
+import CustomLoader from "./component/custom-loader";
 import "./App.css";
 
 function App() {
@@ -11,6 +15,22 @@ function App() {
     status: false,
     text: "",
   });
+  const [solutions, setSolutions] = useState([]);
+  const [finished, setFinished] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const init = () => {
+    setVariables([]);
+    setConditions([]);
+    setInputed([]);
+    setNoSolution({
+      status: false,
+      text: "",
+    });
+    setSolutions([]);
+    setFinished(false);
+    setLoading(false);
+  };
 
   const handleFiles = (files) => {
     const reader = new FileReader();
@@ -27,7 +47,7 @@ function App() {
       cons,
       enable,
       noSolution: solutionStatus,
-    } = generateVariablesConditions(ptrs);
+    } = generateVariablesConditions(ptrs, setLoading);
 
     if (!enable) {
       alert("Can't parse current txt file. Please upload another one.");
@@ -41,18 +61,38 @@ function App() {
   };
 
   const calculate = () => {
-    console.log("calculate", variables, conditions);
     if (noSolution && noSolution.status) {
       alert(noSolution.text);
-      setVariables([]);
-      setConditions([]);
-      setInputed([]);
-      setNoSolution({
-        status: false,
-        text: "",
-      });
+      init();
       return;
     }
+    const sols = isEqualCondition(variables, conditions, setLoading);
+    if (sols && sols.length) {
+      setSolutions([...sols]);
+    }
+    setFinished(true);
+  };
+
+  const download = () => {
+    let exportData = [];
+    solutions.forEach((item, index) => {
+      exportData.push({
+        id: index + 1,
+        solution: item,
+      });
+    });
+    handleSaveToPC(exportData);
+    init();
+  };
+
+  const handleSaveToPC = (jsonData) => {
+    const fileData = JSON.stringify(jsonData);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = `solutions.json`;
+    link.href = url;
+    link.click();
   };
 
   return (
@@ -62,14 +102,48 @@ function App() {
           <button>Upload</button>
         </ReactFileReader>
         <button onClick={calculate}>Calculate</button>
+        <button disabled={!solutions.length || !finished} onClick={download}>
+          Download
+        </button>
       </div>
-      {inputed.length ? (
-        <div className="inputed custom-scroll-bar">
-          {inputed.map((item, index) => (
-            <p key={index}>{item}</p>
-          ))}
-        </div>
-      ) : null}
+      <div className="viewer">
+        {inputed.length ? (
+          <div>
+            <p>Import</p>
+            <div className="inputed custom-scroll-bar">
+              {inputed.map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </div>
+          </div>
+        ) : null}
+        {finished ? (
+          <div>
+            <p>Export</p>
+            {solutions.length ? (
+              <div className="exported custom-scroll-bar">
+                {solutions.map((item, index) => (
+                  <div key={index}>
+                    <p>{`solution-${index + 1}`}</p>
+                    {item.map((it, idx) => {
+                      return (
+                        <p key={`${index}-${idx}`}>
+                          {`${it.name} = ${it.value}`}
+                        </p>
+                      );
+                    })}
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="inputed custom-scroll-bar">
+                <i>Your eqns does not have right solution.</i>
+              </div>
+            )}
+          </div>
+        ) : null}
+      </div>
+      {loading ? <CustomLoader loaded={loading} /> : null}
     </div>
   );
 }
--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -1,6 +1,10 @@
 import _, { isEmpty } from "lodash";
 
-export const generateVariablesConditions = (ptrs) => {
+export const generateVariablesConditions = (ptrs, setLoading) => {
+  if (setLoading) {
+    setLoading(true);
+  }
+
   let result = {
     vars: [],
     cons: [],
@@ -18,6 +22,10 @@ export const generateVariablesConditions = (ptrs) => {
   ) {
     console.log("Can't parse current txt file.");
     result.enable = false;
+
+    if (setLoading) {
+      setLoading(false);
+    }
     return result;
   }
 
@@ -92,23 +100,36 @@ export const generateVariablesConditions = (ptrs) => {
   }
 
   if (result.noSolution.status) {
+    if (setLoading) {
+      setLoading(false);
+    }
     return result;
   }
 
-  const value = {
-    variables: newVariables,
-    conditions: newConditions,
-    result,
-  };
-  result = sortBy0(value);
+  result.vars = newVariables;
+  result.cons = newConditions;
+
+  result = sortBy0(result);
+  if (result.noSolution.status) {
+    if (setLoading) {
+      setLoading(false);
+    }
+    return result;
+  }
+
+  result = sortBy1(result);
+
+  if (setLoading) {
+    setLoading(false);
+  }
 
   return result;
 };
 
-const sortBy0 = (value) => {
-  const { variables, conditions, result } = value;
+const sortBy0 = (result) => {
+  const { vars: variables, cons: conditions } = result;
 
-  let newResult = _.cloneDeep(result),
+  const newResult = _.cloneDeep(result),
     newVariables = _.cloneDeep(variables);
 
   const sortConditions1 = [];
@@ -175,11 +196,20 @@ const sortBy0 = (value) => {
       for (let i = 0; i < steps; i++) {
         const it = content.substring(i * 3, (i + 1) * 3);
         const itItem = _.find(newVariables, { name: it });
-        if (!isEmpty(itItem) && itItem.value === 0) {
+
+        if (!itItem || isEmpty(itItem)) {
+          ignore = true;
+          newResult.noSolution = {
+            status: true,
+            text: `"${it}" is not existed in variables.`,
+          };
+          break;
+        } else if (itItem.value === 0) {
           ignore = true;
           break;
         }
       }
+
       if (!ignore) {
         sortConditions1.push(item);
       }
@@ -195,9 +225,11 @@ const sortBy0 = (value) => {
     if (item.value && item.contents.length === 1) {
       const content = item.contents[0],
         steps = content.length / 3;
+
       for (let i = 0; i < steps; i++) {
         const it = content.substring(i * 3, (i + 1) * 3);
         const itIndex = _.findIndex(newVariables, { name: it });
+
         if (itIndex > -1) {
           newVariables[itIndex].value = 1;
         } else {
@@ -216,4 +248,175 @@ const sortBy0 = (value) => {
   newResult.cons = sortConditions2;
 
   return newResult;
+};
+
+const sortBy1 = (result) => {
+  const { vars: variables, cons: conditions } = result;
+
+  const newResult = _.cloneDeep(result),
+    newVariables = _.cloneDeep(variables);
+
+  const newConditions = [];
+  conditions.forEach((item) => {
+    if (item.value) {
+      const newItem = {
+        contents: [],
+        value: item.value,
+      };
+      let ignore = false;
+      item.contents.forEach((it) => {
+        if (it && it.length === 3) {
+          const itIndex = _.findIndex(newVariables, { name: it });
+          if (itIndex > -1 && newVariables[itIndex].value === 0) {
+            ignore = true;
+          }
+        } else {
+          const filtered = _.filter(conditions, (o) => o.value === 0);
+          if (filtered && filtered.length) {
+            const matchIndex = _.findIndex(
+              filtered,
+              (o) => o.contents[0] === it
+            );
+            if (matchIndex > -1) {
+              ignore = true;
+            }
+          }
+        }
+        if (!ignore) {
+          newItem.contents.push(it);
+        }
+      });
+      newConditions.push(newItem);
+    } else {
+      let content = item.contents[0],
+        newContent = "";
+      const steps = content.length / 3;
+      for (let i = 0; i < steps; i++) {
+        const it = content.substring(i * 3, (i + 1) * 3);
+        const itIndex = _.findIndex(newVariables, { name: it });
+        if (itIndex > -1 && newVariables[itIndex].value === -1) {
+          newContent += it;
+        }
+      }
+      newConditions.push({
+        contents: [newContent],
+        value: 0,
+      });
+    }
+  });
+
+  newResult.vars = newVariables;
+  newResult.cons = newConditions;
+
+  return newResult;
+};
+
+export const isEqualCondition = (variables, conditions, setLoading) => {
+  if (setLoading) {
+    setLoading(true);
+  }
+
+  const solutions = [];
+  let counter = 0;
+
+  const noValIndexes = _.filter(
+    _.range(variables.length),
+    (i) => variables[i].value === -1
+  );
+
+  if (noValIndexes && noValIndexes.length) {
+    const maxCounter = Math.min(Math.pow(2, noValIndexes.length), 1024);
+    // console.log("noValIndexes", noValIndexes, maxCounter);
+
+    while (counter < maxCounter) {
+      const newVariables = _.cloneDeep(variables);
+      for (let i = 0; i < noValIndexes.length; i++) {
+        const modular = Math.pow(2, i + 1),
+          itIndex = noValIndexes[i];
+        if (counter % modular >= modular / 2) {
+          newVariables[itIndex].value = 0;
+        } else {
+          newVariables[itIndex].value = 1;
+        }
+      }
+      counter += 1;
+      // console.log("newVariables", newVariables);
+
+      if (!conditions.length) {
+        solutions.push(_.cloneDeep(newVariables));
+      } else if (isRightForConditions(newVariables, conditions)) {
+        solutions.push(_.cloneDeep(newVariables));
+      }
+    }
+  } else {
+    // console.log("all variables have been defined already", variables);
+
+    if (!conditions.length) {
+      solutions.push(_.cloneDeep(variables));
+    } else if (isRightForConditions(variables, conditions)) {
+      solutions.push(_.cloneDeep(variables));
+    }
+  }
+
+  if (setLoading) {
+    setLoading(false);
+  }
+
+  return solutions;
+};
+
+const isRightForConditions = (variables, conditions) => {
+  let result = true;
+
+  for (let i = 0; i < conditions.length; i++) {
+    const condition = conditions[i];
+    if (!isSolutionForCondition(variables, condition)) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
+};
+
+const isSolutionForCondition = (variables, condition) => {
+  let result = true,
+    leftTotal = 0;
+
+  condition.contents.forEach((item) => {
+    leftTotal += convertNameToValue(variables, item);
+  });
+
+  if (condition.value === 0 && leftTotal !== condition.value) {
+    result = false;
+  } else if (condition.value === 1 && leftTotal < 1) {
+    result = false;
+  }
+
+  // console.log("isSolutionForCondition", variables, condition, leftTotal);
+  return result;
+};
+
+const convertNameToValue = (variables, value) => {
+  if (!value || value.length % 3 !== 0) return -1000;
+
+  const steps = value.length / 3;
+  let result = 1,
+    error = false;
+
+  for (let i = 0; i < steps; i++) {
+    const name = value.substring(i * 3, (i + 1) * 3);
+    const nameIndex = _.findIndex(variables, { name: name });
+    if (nameIndex > -1) {
+      result *= variables[nameIndex].value;
+    } else {
+      error = true;
+    }
+  }
+
+  if (error) {
+    return -1000;
+  } else {
+    return result;
+  }
 };
